016ae2d7e2e912e6b65e09fa9c8166f53e157b24
Pavel Kochetkov <kochetkov_p@mail.ru>
2022-12-17 20:45:21 +0300

    contact page change

--- src/components/Sendmessage.js
+++ src/components/Sendmessage.js
@@ -5,8 +5,10 @@ export const Sendmessage = (props) => {
     return (
         <React.Fragment>
             <div className="media">
-                <a href={props.contact.url}><img src={props.contact.img} alt={props.contact.name}/></a>
-                <div className="title_sl">{props.contact.title}</div>
+                <a href={props.contact.url} target='_blank' rel="noreferrer">
+                    <div className="title_sl">{props.contact.title}</div>
+                    <img src={props.contact.img} alt={props.contact.name}/>
+                </a>
             </div>
         </React.Fragment>
     )
