f9c51803c5bf1f3240fd3f828206f4ba987e055f
Pavel Kochetkov <kochetkov_p@mail.ru>
2022-12-25 21:07:18 +0300

    changing styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import '../css/home.css'
 
 export const Home = () => {
     return(
-        <div class="d-flex align-items-center min-vh-100">
+        <div class="d-flex">
             <div class="home container text-center">
                 <h2>Павел Кочетков - Beginner programmer</h2>
             </div>
